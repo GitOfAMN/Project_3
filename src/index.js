ef62be2964b0dae71cb549ca38dd18f6a0e92192
GitOfAMN <ashleynickens@gmail.com>
2023-01-04 22:07:31 -0500

    functioning frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+// index.js
+import './styles.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App/App';
 
-ReactDOM.render(
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
