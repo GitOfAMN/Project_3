db3db1c8fae132f48349f2c5d5e2b6ea1221f9a5
GitOfAMN <ashleynickens@gmail.com>
2023-01-10 21:33:47 -0500

    final

--- config/seed.js
+++ config/seed.js
@@ -8,12 +8,10 @@ const Item = require('../models/item');
 
   await Category.deleteMany({});
   const categories = await Category.create([
-    { name: 'Alcohol', sortOrder: 10 },
-    { name: 'Food', sortOrder: 20 },
-    { name: 'Health', sortOrder: 40 },
-    { name: 'Cleaning', sortOrder: 50 },
-    { name: 'Baby', sortOrder: 60 },
-    { name: 'Pets', sortOrder: 70 },
+    { name: 'Spirits', sortOrder: 10 },
+    { name: 'Beer', sortOrder: 20 },
+    { name: 'Wine', sortOrder: 40 },
+    { name: 'Accessories', sortOrder: 50 },
   ]);
 
   await Item.deleteMany({});
@@ -22,24 +20,17 @@ const Item = require('../models/item');
     { name: 'Casamigos Reposado', img: 'https://i.imgur.com/2bdYcQz.jpg', category: categories[0], price: 57.99 },
     { name: 'Casamigos Blanco', img: 'https://i.imgur.com/l03teVi.jpg', category: categories[0], price: 39.99 },
     {name: 'Casamigos Añejo', img: 'https://i.imgur.com/vnIsLPE.jpg', category: categories[0], price: 61.99},
-    {name: 'Ben & Jerry\'s Pistachio Ice Cream ', img: 'https://i.imgur.com/qvixQ23.jpg', category: categories[1], price: 6.99},
-    {name: 'Skinny Pop', img: 'https://i.imgur.com/pHwJdgA.jpg', category: categories[1], price: 5.49},
-    {name: 'PopCorners', img: 'https://i.imgur.com/h7kl7Pk.jpg', category: categories[1], price: 4.49},
-    {name: 'Oero Cookies', img: 'https://i.imgur.com/NpIndoA.jpg', category: categories[1], price: 4.95},
-    // {name: '', img: '', category: categories[3], price: 3.95},
-    // {name: '', img: '', category: categories[3], price: 7.95},
-    // {name: '', img: '', category: categories[3], price: 1.95},
-    // {name: '', img: '', category: categories[4], price: 2.95},
-    // {name: '', img: '', category: categories[4], price: 3.95},
-    // {name: '', img: '', category: categories[5], price: 1.95},
-    // {name: '', img: '', category: categories[5], price: 0.95},
-    // {name: '', img: '', category: categories[5], price: 2.95},
-    // {name: '', img: '', category: categories[5], price: 3.95},
-    // {name: '', img: '', category: categories[6], price: 0.95},
-    // {name: '', img: '', category: categories[6], price: 0.95},
-    // {name: '', img: '', category: categories[6], price: 8.95},
-    // {name: '', img: '', category: categories[6], price: 3.95},
-    // {name: '', img: '', category: categories[6], price: 7.95},
+    {name: 'Blue Moon', img: 'https://i.imgur.com/XjJdZC5.png', category: categories[1], price: 9.99},
+    {name: 'La Rubia', img: 'https://i.imgur.com/VkmfR45.png', category: categories[1], price: 12.99},
+    {name: 'Peroni', img: 'https://i.imgur.com/Ecntv3J.jpg', category: categories[1], price: 13.99},
+    {name: 'Duvel', img: 'https://i.imgur.com/7LS9EOz.png', category: categories[1], price: 14.99},
+    {name: 'Belle Glos', img: 'https://i.imgur.com/sHxwB1r.jpg', category: categories[2], price: 59.99},
+    {name: 'Caymus', img: 'https://i.imgur.com/c6txK0f.gif', category: categories[2], price: 79.99},
+    {name: 'Whispering Angel', img: 'https://i.imgur.com/pbsXcwd.jpg', category: categories[2], price: 17.95},
+    {name: 'Miraval', img: 'https://i.imgur.com/rhmP4Hh.jpg', category: categories[2], price: 19.99},
+    {name: 'Houdini Electric Cork Screw', img: 'https://i.imgur.com/E8aAmzl.jpg', category: categories[3], price: 21.99},
+    {name: 'Decanter', img: 'https://i.imgur.com/yB0w84L.jpg', category: categories[3], price: 99.99},
+    {name: 'Drinkware Set', img: 'https://i.imgur.com/Aqnyp0I.jpg', category: categories[3], price: 89.99},
   ]);
 
   console.log(items)
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,16 +4,67 @@ import LoginForm from '../../components/LoginForm/LoginForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import Logo from '../../components/Logo/Logo';
 
+
+
 export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
+  const [swap, setSwap] = useState(false);
+
 
   return (
     <main className={styles.AuthPage}>
-      <div>
-        <Logo />
-        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
-      </div>
-      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
-    </main>
+      {
+        swap ?
+          <>
+            <div>
+              <Logo />
+              <h3 onClick={() => setShowLogin(!showLogin)}>{!showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+            </div>
+            {!showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+          </>
+          :
+          <>
+            <div>
+              <Logo />
+              <h3 onClick={() => setSwap(true)}>ENTER</h3>
+            </div>
+          </>
+      }
+    </main >
   );
 }
+
+
+
+// export default function AuthPage({ setUser }) {
+//   const [showLogin, setShowLogin] = useState(true);
+//   const [load, setLoad] = useState(false);
+
+//   const handleClick = (bool) => {
+//     setLoad(true);
+//     setShowLogin(bool);
+//   };
+
+//   return (
+//     <main className={styles.AuthPage}>
+//       <div>
+//         <Logo />
+//         {!load ? (
+//           <div>
+//             <h3 onClick={() => handleClick(true)}>LOGIN</h3>
+//             <h3 onClick={() => handleClick(false)}>SIGN UP</h3>
+//           </div>
+//         ) : (
+//           <h3 onClick={() => setShowLogin(!showLogin)}>
+//             {showLogin ? "SIGN UP" : "LOG IN"}
+//           </h3>
+//         )}
+//       </div>
+//       {showLogin ? (
+//         <LoginForm setUser={setUser} />
+//       ) : (
+//         <SignUpForm setUser={setUser} />
+//       )}
+//     </main>
+//   );
+// }
